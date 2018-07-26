290ad88118a0605fabc213c97c1cfd3d9d3ae718
Morten Bek Ditlevsen <morten@ka-ching.dk>
2018-07-26 14:10:12 +0200

    Added decoding extension

--- SwiftyFirebase/FirebaseExtensions.swift
+++ SwiftyFirebase/FirebaseExtensions.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import FirebaseDatabase
+import Result
+
+public typealias DecodeResult<T> = Result<T, DecodeError>
+
+public enum DecodeError: Error {
+    case noValuePresent
+    case conversionError(Error)
+}
+
+extension DataSnapshot {
+    func decoded<T>() -> DecodeResult<T> where T: Decodable {
+        guard exists(), let value = value else {
+            return Result.failure(DecodeError.noValuePresent)
+        }
+        let decoder = StructureDecoder()
+        do {
+            let d = try decoder.decode(T.self, from: value)
+            return Result.success(d)
+        } catch {
+            return Result.failure(DecodeError.conversionError(error))
+        }
+    }
+}
+
+public extension DatabaseQuery {
+    func observeSingleEvent<T>(of type: DataEventType,
+                               with block: @escaping (DecodeResult<T>) -> Void)
+        where T: Decodable {
+            observeSingleEvent(of: type) { snap in
+                block(snap.decoded())
+            }
+    }
+
+    func observe<T>(eventType: DataEventType,
+                    with block:  @escaping (DecodeResult<T>) -> Void) -> UInt
+        where T: Decodable {
+            return observe(eventType, with: { snap in
+                block(snap.decoded())
+            })
+    }
+}
