d68a9b3af69c7017cd3af2db2cf5d7f9d959510d
Morten Bek Ditlevsen <morten@ka-ching.dk>
2018-07-26 14:06:05 +0200

    Added external dependencies

---
+++ SwiftyFirebase/FirebaseExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  FirebaseExtensions.swift
+//  SwiftyFirebase
+//
+//  Created by Morten Bek Ditlevsen on 26/07/2018.
+//  Copyright © 2018 Ka-ching. All rights reserved.
+//
+
+import Foundation
