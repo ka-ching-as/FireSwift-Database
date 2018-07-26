6a5808bc60c224b56e6fa425d3dcf843328fb8a6
Morten Bek Ditlevsen <morten@ka-ching.dk>
2018-07-26 13:52:34 +0200

    Initial Commit

---
+++ SwiftyFirebase/SwiftyFirebase.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyFirebase.h
+//  SwiftyFirebase
+//
+//  Created by Morten Bek Ditlevsen on 26/07/2018.
+//  Copyright © 2018 Ka-ching. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyFirebase.
+FOUNDATION_EXPORT double SwiftyFirebaseVersionNumber;
+
+//! Project version string for SwiftyFirebase.
+FOUNDATION_EXPORT const unsigned char SwiftyFirebaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyFirebase/PublicHeader.h>
+
+
---
+++ SwiftyFirebaseTests/SwiftyFirebaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftyFirebaseTests.swift
+//  SwiftyFirebaseTests
+//
+//  Created by Morten Bek Ditlevsen on 26/07/2018.
+//  Copyright © 2018 Ka-ching. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftyFirebase
+
+class SwiftyFirebaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
